4c3c25753f4db8f961ca73192e2071f1fbe633b7
rafael.moura <rafaelfelipe.peixoto@gmail.com>
2022-12-21 09:39:25 -0300

    Início do projeto

---
+++ src/test/java/br/com/apitestesunitarios/ApiTestesUnitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.apitestesunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestesUnitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
