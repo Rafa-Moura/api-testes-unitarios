6dd57c79904194cf6b7e3782f60b01e7181d2655
rafael.moura <rafaelfelipe.peixoto@gmail.com>
2022-12-21 21:36:17 -0300

    Adicionando anotação @JsonIgnore no Dto para omitir informações do retorno

--- src/main/java/br/com/apitestesunitarios/controller/dto/UserDto.java
+++ src/main/java/br/com/apitestesunitarios/controller/dto/UserDto.java
@@ -1,5 +1,6 @@
 package br.com.apitestesunitarios.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDto {
+    @JsonIgnore
     private Long id;
     private String name;
     private String email;
+    @JsonIgnore
     private String password;
 }
